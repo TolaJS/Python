8e08a14f1f541419192c9022e302d93aafb9c34c
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2022-05-06 02:02:12 +0100

    Find the next prime number

--- Numbers/next_prime.py
+++ Numbers/next_prime.py
@@ -1,50 +1,54 @@
-'''
+"""
 FIND THE NEXT PRIME NUMBER UNTIL THE USER DECIDES TO STOP
 
 Name = next_prime.py
 Author = Tola Shobande (tolajs)
 Comment: Just practicing how to use generators.
 Reference = 'https://github.com/Silverneo/pylearn/blob/master/prime.py'
-'''
+"""
+
 
 def isprime(value):
-    '''
-isprime(value) ->
-    Returns True if the given parameter value is a prime number.
-    '''
+    """
+    isprime(value) ->
+        Returns True if the given parameter value is a prime number.
+    """
     if value == 2:
         return True
     if value % 2 == 0:
         return False
-    for i in range(3, int(value**0.5)+1, 2):
+    for i in range(3, int(value**0.5) + 1, 2):
         if value % i == 0:
             return False
     return True
 
+
 def gen_prime():
-    '''
-gen_prime() ->
-    Generator function that yields a value given isprime() returns True  
-    '''
+    """
+    gen_prime() ->
+        Generator function that yields a value given isprime() returns True
+    """
     value = 3
     yield 2
-    
-    while(True):
+
+    while True:
         if isprime(value):
             yield value
         value += 2
-        
+
+
 def main():
     print("Press Enter to generate the next prime number or Q to quit.")
     g = gen_prime()
-    
-    while(True):
-        entry = input('-> ')
-        
-        if entry.lower() == 'q':
+
+    while True:
+        entry = input("-> ")
+
+        if entry.lower() == "q":
             break
         else:
             print(next(g))
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
